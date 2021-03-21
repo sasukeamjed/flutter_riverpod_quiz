1219a677c38ce46393259b62db865063f4b3a17f
sasuk <sasukeamjed@gmail.com>
2021-03-21 19:57:50 +0400

    1.2 commit

--- lib/repositories/quiz/base_quiz_repository.dart
+++ lib/repositories/quiz/base_quiz_repository.dart
@@ -1,5 +1,6 @@
-
+import 'package:flutter_riverpod_quiz/enums/difficulty.dart';
+import 'package:flutter_riverpod_quiz/models/question_model.dart';
 
 abstract class BaseQuizRepository{
-  Future<List<Question>> getQuestions(int numQuestions, int gategoryId, Difficulty difficulty);
+  Future<List<Question>> getQuestions(int numQuestions, int categoryId, Difficulty difficulty);
 }
