693e06af90c5ff745398f1b3d17c9422cdb4e283
sasuk <sasukeamjed@gmail.com>
2021-03-24 23:24:49 +0400

    finished app

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,11 @@ void main() {
   runApp(MyApp());
 }
 
+//Question:
+// 1- what is HtmlCharacterEntities
+// 2- what is flexible widget
+// 3- check what is SizedBox.shrink()
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -89,7 +94,7 @@ class QuizScreen extends HookWidget {
               if (pageController.page.toInt() + 1 < questions.length) {
                 pageController.nextPage(
                     duration: const Duration(milliseconds: 250),
-                    curve: Curves.linear)
+                    curve: Curves.linear);
               }
             });
           },
@@ -179,13 +184,14 @@ class QuizQuestions extends StatelessWidget {
             ),
             Column(
               children: question.answers.map((e) =>
-                  AnswerCard(answer: e,
+                  AnswerCard(
+                    answer: e,
                     isSelected: e == state.selectedAnswer,
                     isCorrect: e == question.correctAnswer,
-                    isDisplyingAnswer: state.answered,
+                    isDisplayingAnswer: state.answered,
                     onTap: () =>
                         context.read(quizControllerProvider).submitAnswer(
-                            question, e),)),
+                            question, e),)).toList(),
             ),
           ],
         );
@@ -194,20 +200,85 @@ class QuizQuestions extends StatelessWidget {
   }
 }
 
-class AnswerCard extends StatelessWidget{
+class AnswerCard extends StatelessWidget {
   final String answer;
   final bool isSelected;
   final bool isCorrect;
   final bool isDisplayingAnswer;
   final VoidCallback onTap;
 
-  const AnswerCard({Key key, @required this.answer, @required this.isSelected, @required this.isCorrect, @required this.isDisplayingAnswer, @required this.onTap}) : super(key: key);
+  const AnswerCard(
+      {Key key, @required this.answer, @required this.isSelected, @required this.isCorrect, @required this.isDisplayingAnswer, @required this.onTap})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 12.0,
+          horizontal: 20.0,
+        ),
+        padding: const EdgeInsets.symmetric(
+          vertical: 12.0,
+          horizontal: 20.0,
+        ),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: boxShadow,
+          border: Border.all(
+            color: isDisplayingAnswer ? isCorrect ? Colors.green : isSelected
+                ? Colors.red
+                : Colors.white : Colors.white,
+            width: 4.0,
+          ),
+          borderRadius: BorderRadius.circular(100.0),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              child: Text(HtmlCharacterEntities.decode(answer),
+                style: TextStyle(color: Colors.black,
+                  fontSize: 16.0,
+                  fontWeight: isDisplayingAnswer && isCorrect
+                      ? FontWeight.bold
+                      : FontWeight.w400,),),
+            ),
+            if(isDisplayingAnswer)
+              isCorrect ? const CircularIcon(icon: Icons.check, color: Colors.green) : isSelected
+                  ? const CircularIcon(icon: Icons.close, color: Colors.red,)
+                  : const SizedBox.shrink()
+
+          ],
+        ),
+      ),
+    );
   }
 }
+
+class CircularIcon extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+
+  const CircularIcon({Key key, this.icon, this.color}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24.0,
+      width: 24.0,
+      decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.circle,
+        boxShadow: boxShadow,
+      ),
+      child: Icon(icon, color: Colors.white, size: 16.0,),
+    );
+  }
+}
+
 
 
 class QuizError extends StatelessWidget {
